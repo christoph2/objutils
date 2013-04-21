531c643c2b2c98be072e051350f01c6b523321c8
Christoph Schueler <cpu12.gems@googlemail.com>
2013-04-21 09:37:29 +0200

    Added constructor to 'MOSTec.py'.

--- objutils/MOSTec.py
+++ objutils/MOSTec.py
@@ -30,27 +30,30 @@ __copyright__ = """
 import objutils.HexFile as HexFile
 import cStringIO
 
-DATA=1
-EOF=2
+DATA    = 1
+EOF     = 2
 
 FORMATS=(
-    (DATA,";LLAAAADDCCCC"),
-    (EOF,";00")
+    (DATA,  ";LLAAAADDCCCC"),
+    (EOF,   ";00")
 )
 
 
 class Reader(HexFile.Reader):
-    def checkLine(self,line,formatType):
-        if formatType==DATA:
-            if line.length!=len(line.chunk):
+    def __init__(self, inFile):
+        super(Reader,self).__init__(FORMATS, inFile)
+
+    def checkLine(self, line, formatType):
+        if formatType == DATA:
+            if line.length != len(line.chunk):
                 raise HexFile.InvalidRecordLengthError("Byte count doesn't match length of actual data.")
             # todo: factor out checksum calculation from line!!!
-            checksum=((sum([line.length,(line.address & 0xff00)>>8,line.address & 0xff])+
+            checksum = ((sum([line.length, (line.address & 0xff00) >> 8, line.address & 0xff]) +
                 sum(line.chunk)) & 0xffff)
-            if line.checksum!=checksum:
+            if line.checksum != checksum:
                 raise HexFile.InvalidRecordChecksumError()
 
-    def isDataLine(self,line,formatType):
-        return formatType==DATA
+    def isDataLine(self, line, formatType):
+        return formatType == DATA
 
 
