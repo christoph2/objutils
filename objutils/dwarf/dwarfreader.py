48e44e3a76f15aa40e4e797f34d6602a31a1aea4
Christoph Schueler <cpu12.gems@googlemail.com>
2013-12-01 09:41:48 +0100

    Added class 'DebugSectionReader'. Removed file 'abbreviations.py'.

--- objutils/dwarf/dwarfreader.py
+++ objutils/dwarf/dwarfreader.py
@@ -26,6 +26,8 @@ __copyright__ = """
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
+from collections import namedtuple
+
 try:
     import cStringIO as StringIO
 except ImportError:
@@ -33,6 +35,45 @@ except ImportError:
 import struct
 
 from objutils.readers import PlainBinaryReader
+
+from objutils.dwarf import constants
+
+FORM_READERS = {
+    constants.DW_FORM_string:       'asciiz',
+    constants.DW_FORM_udata:        'uleb',
+    constants.DW_FORM_sdata:        'sleb',
+    constants.DW_FORM_data1:        'u8',
+    constants.DW_FORM_data2:        'u16',
+    constants.DW_FORM_data4:        'u32',
+    constants.DW_FORM_data8:        'u64',
+    constants.DW_FORM_addr:         'u32',  ## TODO: Target word size!!!
+    constants.DW_FORM_block:        'block',
+    constants.DW_FORM_block1:       'block1',
+    constants.DW_FORM_block2:       'block2',
+    constants.DW_FORM_block4:       'block4',
+    constants.DW_FORM_flag:         'u8',
+    constants.DW_FORM_ref_addr:     'u32',
+    constants.DW_FORM_ref1:         'u8',
+    constants.DW_FORM_ref2:         'u16',
+    constants.DW_FORM_ref4:         'u32',
+    constants.DW_FORM_ref8:         'u64',
+    constants.DW_FORM_ref_udata:    'uleb',
+    ###
+    constants.DW_FORM_strp:         '', # TODO: This is a offset into string table (.debug_str)!!!
+    constants.DW_FORM_indirect:     '', # TODO: uleb value, that represents its form!
+    constants.DW_FORM_sec_offset:   '', # This is an offset into the .debug_line/.debug_loc /.debug_macinfo section, p. 162ff
+    constants.DW_FORM_exprloc:      '', # This is an unsigned LEB128 length followed by the number of information
+                                        # bytes specified by the length (DW_FORM_exprloc).
+    constants.DW_FORM_flag_present: '', #  the attribute is implicitly indicated as present, and no value is
+                                        # encoded in the debugging information entry itself
+    constants.DW_FORM_ref_sig8:     'u64',
+}
+
+AbbreviationEntry = namedtuple('Abbreviation', 'tag, children, attrs')
+
+SET_OFFSET      = 1
+IGNORE_OFFSET   = 2
+
 
 class DwarfReader(PlainBinaryReader):
 
@@ -81,4 +122,78 @@ class DwarfReader(PlainBinaryReader):
 
     def block(self):
         return self._block(-1)
+
+
+def makeAttrName(value):
+    return value.replace('TAG','').title().replace('_', '')
+
+
+class DebugSectionReader(object):
+
+    def __init__(self, sections):
+        self.sections = sections
+        self.abbrevs = {}
+
+    def process(self):
+        self.processAbbreviations()
+        self.processInfoSection()
+        #print self.abbrevs
+
+    def processAbbreviations(self):
+        image = self.sections['.debug_abbrev'].image
+        dr = DwarfReader(image)
+        totalSize = len(image)
+        abbrevs = {}
+        abbrevEntries = {}
+        offsetState = SET_OFFSET
+        while dr.pos < totalSize:
+            if offsetState == SET_OFFSET:
+                offset = dr.pos
+                offsetState = IGNORE_OFFSET
+            code = dr.uleb()
+            if code == 0:
+                abbrevs[offset] = abbrevEntries
+                abbrevEntries = {}
+                offsetState = SET_OFFSET
+                continue
+            tagValue = dr.uleb()
+            tag = constants.TAG_MAP.get(tagValue, tagValue)
+            children = dr.u8()
+            attrSpecs = []
+            while True:
+                attrValue = dr.uleb()
+                attr = constants.AttributeEncoding(attrValue)
+                formValue = dr.uleb()
+                form = constants.AttributeForm(formValue)
+                if attrValue == 0 and formValue == 0:
+                    break
+                attrSpecs.append((attr, form))
+            abbrevEntries[code] = AbbreviationEntry(tag, "DW_CHILDREN_yes" if children == constants.DW_CHILDREN_yes else "DW_CHILDREN_no", attrSpecs)
+        self.abbrevs = abbrevs
+
+    def processInfoSection(self):
+        image = self.sections['.debug_info'].image
+        dr = DwarfReader(image)
+        while dr.pos < len(image):
+            length = dr.u32()
+            stopPosition = dr.pos + length
+            dwarfVersion = dr.u16()
+            abbrevOffs = dr.u32()
+            abbrevs = self.abbrevs[abbrevOffs]
+            targetAddrSize = dr.u8()
+
+            while dr.pos < stopPosition:
+                number = dr.uleb()
+                if number == 0:
+                    print "<*** EMPTY ***>"
+                    continue
+                entry = abbrevs[number]
+                print "=" * 80
+                print entry.tag, entry.children
+                print "=" * 80
+                for attr in entry.attrs:
+                    attribute, form = attr
+                    reader = FORM_READERS[form.value]
+                    attrValue = getattr(dr, reader)()
+                    print "%s ==> '%s'" % (attribute, attrValue)
 
