06b835dace3deb287f560477432b310b3300593a
Christoph Schueler <cpu12.gems@googlemail.com>
2015-02-09 10:55:36 +0100

    Added missing file 'registry.py'.

---
+++ objutils/registry.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.0"
+
+__copyright__ = """
+    pyObjUtils - Object file library for Python.
+
+   (C) 2010-2014 by Christoph Schueler <cpu12.gems@googlemail.com>
+
+   All Rights Reserved
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+from collections import namedtuple
+
+Codec = namedtuple("Codec", "reader writer")
+codecs = {}
+
+def registry():
+    return codecs
+
+def getFormats():
+    return sorted(codecs.keys())
+
+def getCodec(codecName):
+    codec = codecs.get(codecName, None)
+    if codec is None:
+        raise AttributeError("Codec '%s' doesn't exist." % codecName)
+    return codec
+
+def register(name, readerClass, writerClass):
+    if name in codecs:
+        raise AttributeError("Duplicate codec name '%s'." % name)
+    codecs[name] = Codec(readerClass(), writerClass())
+
+
