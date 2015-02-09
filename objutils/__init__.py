8c29a1f1fbecb7ad20f9e283faaf646d46c6fac3
Christoph Schueler <cpu12.gems@googlemail.com>
2015-02-09 10:50:29 +0100

    Removed temporaries.

--- objutils/__init__.py
+++ objutils/__init__.py
@@ -6,34 +6,6 @@ __version__ = "0.1.0"
 __copyright__ = """
     pyObjUtils - Object file library for Python.
 
-   (C) 2010-2013 by Christoph Schueler <cpu12.gems@googlemail.com>
-
-   All Rights Reserved
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-"""
-
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = "0.1.0"
-
-__copyright__ = """
-    pyObjUtils - Object file library for Python.
-
    (C) 2010-2015 by Christoph Schueler <cpu12.gems@googlemail.com>
 
    All Rights Reserved
@@ -57,25 +29,21 @@ from collections import namedtuple
 
 from objutils.registry import getCodec
 
+##
+##  Interface to OBJUTILS.
+##
 def load(codecName, *args, **kws):
-    reader = getCodec(codecName).reader
-
-    return reader.load(*args, **kws)
+    return getCodec(codecName).reader.load(*args, **kws)
 
 def loads(codecName, *args, **kws):
-    reader = getCodec(codecName).reader
-
-    return reader.loads(*args, **kws)
+    return getCodec(codecName).reader.loads(*args, **kws)
 
 def dump(codecName, *args, **kws):
-    writer = getCodec(codecName).writer
-
-    writer.dump(*args, **kws)
+    getCodec(codecName).writer.dump(*args, **kws)
 
 def dumps(codecName, *args, **kws):
-    writer = getCodec(codecName).writer
+    return getCodec(codecName).writer.dumps(*args, **kws)
 
-    return writer.dumps(*args, **kws)
 
 def _importer():
     import objutils.Sig
