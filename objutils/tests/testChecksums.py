e00232116200cd160427c735b29d6611a6990b82
Christoph Schueler <cpu12.gems@googlemail.com>
2015-12-01 08:21:54 +0100

    Some more checksum tests.

--- objutils/tests/testChecksums.py
+++ objutils/tests/testChecksums.py
@@ -4,10 +4,10 @@ import unittest
 from objutils.checksums import nibbleSum, lrc
 from objutils.checksums import COMPLEMENT_NONE, COMPLEMENT_ONES, COMPLEMENT_TWOS
 
-print lrc(range(10), 8, COMPLEMENT_NONE)
-print lrc(range(100), 8, COMPLEMENT_NONE)
+#print lrc(range(10), 8, COMPLEMENT_TWOS)
+#print lrc(range(100), 8, COMPLEMENT_TWOS)
 
-class TestChecksums(unittest.TestCase):
+class TestNiblleSums(unittest.TestCase):
 
     def testNibbleSumCase1(self):
         self.assertEquals(nibbleSum(range(10)), 45)
@@ -15,6 +15,30 @@ class TestChecksums(unittest.TestCase):
     def testNibbleSumCase2(self):
         self.assertEquals(nibbleSum(range(100)), 222)
 
+    # check that s.split fails when the separator is not a string
+    #with self.assertRaises(TypeError):
+    #    s.split(2)
+
+
+class TestLRCs(unittest.TestCase):
+
+    def testLRCCase1(self):
+        self.assertEquals(lrc(range(10), 8, COMPLEMENT_NONE), 45)
+
+    def testLRCCase2(self):
+        self.assertEquals(lrc(range(100), 8, COMPLEMENT_NONE), 86)
+
+    def testLRCCase3(self):
+        self.assertEquals(lrc(range(10), 8, COMPLEMENT_ONES), 210)
+
+    def testLRCCase4(self):
+        self.assertEquals(lrc(range(100), 8, COMPLEMENT_ONES), 169)
+
+    def testLRCCase5(self):
+        self.assertEquals(lrc(range(10), 8, COMPLEMENT_TWOS), 211)
+
+    def testLRCCase6(self):
+        self.assertEquals(lrc(range(100), 8, COMPLEMENT_TWOS), 170)
 
 if __name__ == '__main__':
     unittest.main()
