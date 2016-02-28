47622f16d032570967dd01a95019c43ebfc20023
Christoph Schueler <cpu12.gems@googlemail.com>
2016-02-28 15:51:31 +0100

    logger.py: Added 'debug()' method.

--- objutils/logger.py
+++ objutils/logger.py
@@ -61,6 +61,9 @@ class Logger(object):
     def warn(self, message):
         self.log(message, logging.WARN)
 
+    def debug(self, message):
+        self.log(message, logging.DEBUG)
+
     def error(self, message):
         self.log(message, logging.ERROR)
 
@@ -77,6 +80,7 @@ class Logger(object):
         LEVEL_MAP = {
             "INFO": logging.INFO,
             "WARN": logging.WARN,
+            "DEBUG": logging.DEBUG,
             "ERROR": logging.ERROR,
             "CRITICAL": logging.CRITICAL,
         }
