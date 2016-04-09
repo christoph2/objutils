06e803f6ef9e42096cd77049b95a5bc33ad4c077
Christoph Schueler <cpu12.gems@googlemail.com>
2016-04-09 15:33:45 +0200

    Added 'testFPC.py'.

---
+++ objutils/tests/testFPC.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from objutils import loads, dumps
+from objutils.Segment import Segment
+from objutils.Image import Image
+import unittest
+
+
+SREC = """S123B000576F77212044696420796F75207265616C6C7920676F207468726F7567682061DF
+S120B0206C6C20746861742074726F75626C6520746F207265616420746869733F73
+S5030002FA"""
+
+FPC = """$kL&@h%%,:,B.\?00EPuX0K3rO0JI))
+$;UPR'%%,:<Hn&FCG:at<GVF(;G9wIw
+$7FD1p%%,:LHmy:>GTV%/KJ7@GE[kYz
+$B[6\;%%,:\KIn?GFWY/qKI1G5:;-_e
+$%%%%%"""
+
+fromSrec =  loads('srec', SREC)
+dataFromSRec = dumps('fpc', fromSrec)
+
+fromFPC =  loads('fpc', FPC)
+dataFromFPC = dumps('srec', fromFPC, rowLength = 32)
+
+
+class TestRoundTrip(unittest.TestCase):
+
+    def testFromSrec(self):
+        self.assertEquals(dataFromSRec, FPC)
+
+    def testFromFPC(self):
+        self.assertEqual(dataFromFPC, SREC)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
