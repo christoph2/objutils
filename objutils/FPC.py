f21e84ff21a2db7ed00ab9012f15a570331963ee
Christoph Schueler <cpu12.gems@googlemail.com>
2016-04-12 20:21:15 +0200

    FPC.py: Add call to 'decode()'.

--- objutils/FPC.py
+++ objutils/FPC.py
@@ -74,7 +74,11 @@ class Reader(HexFile.Reader):
                 value = self.convertQuintuple(quintuple)
                 values.append("%08X" % value)
             outLines.append(''.join(values))
-        return super(Reader, self).read(createStringBuffer(bytes('\n'.join(outLines), "ascii")))
+        return super(Reader, self).read(
+            createStringBuffer(
+                bytes('\n'.join(outLines)).decode("ascii")
+            )
+        )
 
     def convertQuintuple(self, quintuple):
         res = 0         # reduce(lambda accu, x: (accu * 85) + x, value, 0)
