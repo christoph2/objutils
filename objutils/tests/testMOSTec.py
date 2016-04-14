933af57a8982c3aa7f869d1a44dceefe45303e12
Christoph Schueler <cpu12.gems@googlemail.com>
2016-04-14 19:09:59 +0200

    Added 'testMOSTec.py'

---
+++ objutils/tests/testMOSTec.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from objutils import loads, dumps
+from objutils.Segment import Segment
+from objutils.Image import Image, Builder
+import unittest
+
+
+MOSTEC = """;10B000576F77212044696420796F75207265610624
+;10B0106C6C7920676F207468726F756768206106B9
+;10B0206C6C20746861742074726F75626C652006C6
+;0DB030746F207265616420746869733F05A3
+;00"""
+
+S19 = """S113B000576F77212044696420796F7520726561D8
+S113B0106C6C7920676F207468726F756768206143
+S113B0206C6C20746861742074726F75626C652036
+S110B030746F207265616420746869733F59
+S5030004F8"""
+
+
+class TestRoundtrip(unittest.TestCase):
+
+    def testLoadsWorks(self):
+        data = loads("mostec", MOSTEC)
+        #data.hexdump()
+        #print(dumps("srec", data))
+        self.assertEqual(dumps("srec", data), S19)
+        
+    def testDumpsWorks(self):
+        data = loads("srec", S19)
+        self.assertEqual(dumps("mostec", data), MOSTEC)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+
