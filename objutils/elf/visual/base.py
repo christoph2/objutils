47cfb4c4f081736823b226ed1a8ae258c1a34815
Christoph Schueler <cpu12.gems@googlemail.com>
2016-06-14 14:51:45 +0100

    Rework readelf functionality.

---
+++ objutils/elf/visual/base.py
@@ -0,0 +1,43 @@
+
+
+class BaseClass(object):
+
+  def __init__(self):
+    pass
+    
+  def join(self, lines, lineSeparator = '\n'):
+    """Make a printable chunk from a bunch of lines.
+    :param lines:  Lines to be joined.
+    :type lines: `iteratable` containing strings and the like.
+    :param lineSeparator: As the name implies, a character or string used to separate lines.
+    """
+    if not hasattr(lines, '__iter__'):
+      raise TypeError("'lines' must be iteratable.")
+    return lineSeparator.join(lines)
+    
+##
+## Tests.    
+##
+import unittest
+
+class TestVisualBaseClass(unittest.TestCase):
+
+  def testAnIterableIsNeededPass(self):
+    bc = BaseClass()
+    self.assertEqual(bc.join(("A", "B", "C")), "A\nB\nC")
+    
+  def testAnIteratableIsNeededFail(self):
+    bc = BaseClass()
+    self.assertRaises(TypeError, bc.join, 4711)
+
+
+  def testLineSeparatorWorks(self):
+    bc = BaseClass()
+    self.assertEqual(bc.join(['X', 'Y', 'Z', lineSeparator = '++']), "X++Y++Z")
+
+def main():
+  unittest.main()
+  
+  
+if __name__ == '__main__':
+  main()
