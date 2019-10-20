100a1fd941379b14880f27239db813b3077072f5
Christoph Schueler <cpu12.gems@googlemail.com>
2019-10-20 06:42:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ from setuptools import setup, find_packages
 from glob import glob
 import sys
 
-def packagez(base):
-    return  ["{0!s}{1!s}{2!s}".format(base, os.path.sep, p) for p in find_packages(base)]
+with open(os.path.join('objutils', 'version.py'), 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.split('=')[-1].strip().strip('"')
+            break
 
 install_reqs = ['future', 'mako', 'six', 'construct', 'attrs']
 
@@ -15,13 +18,22 @@ if sys.version_info.major == 2 or (sys.version_info.major == 3 and sys.version_i
 
 setup(
     name = 'objutils',
-    version = '0.9.0',
+    version = version,
     provides = ['objutils'],
     description = "Objectfile library for Python",
     author = 'Christoph Schueler',
     author_email = 'cpu12.gems@googlemail.com',
     url = 'http://github.com/christoph2/objutils',
-    packages = packagez('objutils'),
+    packages=find_packages(),
+    include_package_data=True,
+    extras_require={
+       "docs": [
+           'sphinxcontrib-napoleon'
+       ],
+        "develop": [
+            "bumpversion"
+       ]
+    } ,
     install_requires = install_reqs,
     #entry_points = {
     #    'console_scripts': [
@@ -35,7 +47,31 @@ setup(
     #    ('objutils/tests/ELFFiles', glob('objutils/tests/ELFFiles*.*')),
     #],
     package_dir = {'tests': 'objutils/tests'},
+    license='GPLv2',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
     #package_data = {'tests': ['ELFFiles/*.*']},
-    #include_package_data = True,
 )
 
