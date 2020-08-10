82c6bcf2d2742abc836b2acd4afed5cad98895a5
Christoph Schueler <cpu12.gems@googlemail.com>
2020-08-10 08:04:37 +0200

    Bump version: 0.3.0 → 0.3.1

--- objutils/__init__.py
+++ objutils/__init__.py
@@ -15,7 +15,7 @@ Registers CODECS and implements an interface to them.
 The first parameter is always the codec name.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __copyright__ = """
     objutils - Object file library for Python.
--- objutils/version.py
+++ objutils/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """ objutils version module """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
