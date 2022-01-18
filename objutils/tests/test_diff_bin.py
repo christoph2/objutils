f145616ef0dec236ddb59fc80c8aed2094f28245
Christoph Schueler <cpu12.gems@googlemail.com>
2022-01-18 08:28:55 +0100

    Add simple bin-diff utility

---
+++ objutils/tests/test_diff_bin.py
@@ -0,0 +1,50 @@
+
+import copy
+import string
+
+from objutils.utils.diff import (
+    BinDiff,
+    diff_bin,
+)
+
+
+def test_modify1():
+    b0 = bytearray(range(256))
+    b1 = copy.copy(b0)
+    b1[126] = 11
+    b1[127] = 12
+    b1[128] = 13
+    b1[136] = 14
+    res = diff_bin(b0, b1)
+    assert res == [
+        BinDiff(type='M', offset=126, a=bytearray(b'~\x7f\x80'),
+        b=bytearray(b'\x0b\x0c\r')),
+        BinDiff(type='M', offset=136, a=bytearray(b'\x88'),
+        b=bytearray(b'\x0e'))
+    ]
+
+def test_modify2():
+    let0= bytearray(string.ascii_letters, "ascii")
+    let1=copy.copy(let0)
+    let1[20:25] = b"hello"
+    let1[45:50] = b"world"
+    res = diff_bin(let0, let1)
+    assert res == [
+        BinDiff(type='M', offset=20, a=bytearray(b'uvwxy'),
+        b=bytearray(b'hello')),
+        BinDiff(type='M', offset=45, a=bytearray(b'TUVWX'),
+        b=bytearray(b'world'))
+    ]
+
+def test_append():
+    h0 = bytes("hello", "ascii")
+    h1 = bytes("hello world", "ascii")
+    res = diff_bin(h0, h1)
+    assert res == [BinDiff(type='A', offset=5, a=None, b=b' world')]
+
+def test_delete():
+    h1 = bytes("hello", "ascii")
+    h0 = bytes("hello world", "ascii")
+    res = diff_bin(h0, h1)
+    assert res == [BinDiff(type='D', offset=5, a=b' world', b=None)]
+
---
+++ objutils/utils/diff.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.0"
+
+__copyright__ = """
+    objutils - Object file library for Python.
+
+   (C) 2022 by Christoph Schueler <cpu12.gems@googlemail.com>
+
+   All Rights Reserved
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+from collections import namedtuple
+
+"""BinDiff
+
+Represents a difference between two binaries.
+
+Attributes
+----------
+
+    type: str
+        Type of Modification:
+
+        - 'A' something was appended.
+        - 'D' something was deleted.
+        - 'M' a modification happend.
+
+    offset: int
+        Location
+
+    a: bytes or bytearray
+        Considered as the original.
+
+    b: bytes or bytearray
+        Considered as the modification.
+"""
+BinDiff = namedtuple("BinDiff", "type offset a b")
+
+
+def diff_bin(a, b):
+    """Simplicistic binary diff algorithm.
+
+    Parameters
+    ----------
+    a: bytes or bytearray
+        Considered as the original.
+
+    b: bytes or bytearray
+        Considered as the modification.
+
+    Returns
+    list of `BinDiff`s.
+    -------
+    """
+    if not isinstance(a, (bytes, bytearray)):
+        raise TypeError("Parameter `a` must be of type `bytes` or `bytearray`")
+    if not isinstance(b, (bytes, bytearray)):
+        raise TypeError("Parameter `b` must be of type `bytes` or `bytearray`")
+    la, lb = len(a), len(b)
+    length = min(la, lb)
+    tmp = []
+    start_offset = 0
+    diff_bytes_a, diff_bytes_b= bytearray(), bytearray()
+    for offset in range(length):
+        ai = a[offset]
+        bi = b[offset]
+        diff = ai - bi
+        if diff:
+            if not diff_bytes_a:
+                start_offset = offset
+            diff_bytes_a.append(ai)
+            diff_bytes_b.append(bi)
+        else:
+            if diff_bytes_a:
+                tmp.append(BinDiff("M", start_offset, diff_bytes_a, diff_bytes_b))
+                diff_bytes_a, diff_bytes_b= bytearray(), bytearray()
+    if lb > la:
+        tmp.append(BinDiff("A", len(a), None, b[-(lb-la) : ]))
+    elif lb < la:
+        tmp.append(BinDiff("D", len(b), a[-(la-lb) : ], None))
+    result = tmp
+    return result
