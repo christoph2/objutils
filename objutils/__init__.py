650562e809b049ffb35f0c24f108db2f0a85e8d8
Christoph Schueler <cpu12.gems@googlemail.com>
2022-01-18 08:29:14 +0100

    Bump version: 0.4.7 → 0.4.8

--- objutils/__init__.py
+++ objutils/__init__.py
@@ -15,7 +15,7 @@ Registers CODECS and implements an interface to them.
 The first parameter is always the codec name.
 """
 
-__version__ = "0.4.7"
+__version__ = "0.4.8"
 
 __copyright__ = """
     objutils - Object file library for Python.
