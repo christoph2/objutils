e93c1da3e7a1b1524a891c4fb3df107c01d27285
Christoph Schueler <cpu12.gems@googlemail.com>
2025-08-19 13:18:30 +0300

    Update doc builder

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,6 @@ release = "0.9"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
-    #    'sphinx.ext.todo'
-    "sphinxcontrib.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -61,7 +59,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
