83e7ba107580b9febf54f18e73480b19f0262571
Christoph Schueler <cpu12.gems@googlemail.com>
2026-02-18 14:39:38 +0200

    ci(build): update build configurations and workflows for broader compatibility
    - Added support for Python 3.14 in `pyproject.toml`.
    - Adjusted `cibuildwheel` `build` and `skip` parameters to refine build targeting.
    - Enhanced GitHub Actions workflow:
      - Introduced `build_wheels` job with OS-specific configurations, including ARM support for macOS and Windows.
      - Updated steps in `build_sdist` and `upload_pypi` jobs with newer action versions for improved reliability.

--- objutils/__init__.py
+++ objutils/__init__.py
@@ -13,7 +13,7 @@ Registers CODECS and implements an interface to them.
 The first parameter is always the codec name.
 """
 
-__version__ = "0.8.10"
+__version__ = "0.8.11"
 
 __all__ = [
     "Image",
